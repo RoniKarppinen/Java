ecdad7aec10d4c1f71597ba411b95385855e6ae2
Roni Karppinen <112978163+Ronttist@users.noreply.github.com>
2023-05-07 13:37:15 +0300

    Update WordList.java

--- Hangman/WordList.java
+++ Hangman/WordList.java
@@ -3,16 +3,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+
 public class WordList {
     // Attributes
     private List<String> words = new ArrayList<String>();
+
     // Constructors
     public WordList(String filename) {
         try {
             Scanner wordreader = new Scanner(new File(filename));
-            while(wordreader.hasNextLine()) {
+            while (wordreader.hasNextLine()) {
                 String scannerstring = wordreader.nextLine().toLowerCase();
-                if(scannerstring != null) {
+                if (scannerstring != null) {
                     words.add(scannerstring);
                 }
             }
@@ -21,31 +23,41 @@ public class WordList {
             System.out.println("File was not found, check the file name and try again.");
         }
     }
+
     public WordList(List<String> words) {
         this.words = words;
     }
+
     // Methods
     /* The method returns words of the wordlist. */
     public List<String> giveWords() {
         return words;
     }
 
-    /* Bonus method, The method returns a new WordList object with only the words whose length corresponds to the value of
-    the variable given as a parameter.  */
+    /*
+     * Bonus method, The method returns a new WordList object with only the words
+     * whose length corresponds to the value of
+     * the variable given as a parameter.
+     */
     public WordList theWordsOfLength(int length) {
         List<String> SameLengthWords = new ArrayList<>();
-        for(int i = 0; i < words.size(); i++) {
-            if(words.get(i).length() == length) {
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).length() == length) {
                 SameLengthWords.add(words.get(i));
             }
         }
         return new WordList(SameLengthWords);
     }
 
-    /*The method returns a new WordList object with only the words with the letters in the exact positions
-    specified in the given string (and matching the length of that given string).
-    For example, the given string is in the format _a_e_ (matching words would be for example camel or panel)
-    where the lines represent any letter and other letters must be in the given positions in the word. */
+    /*
+     * The method returns a new WordList object with only the words with the letters
+     * in the exact positions
+     * specified in the given string (and matching the length of that given string).
+     * For example, the given string is in the format _a_e_ (matching words would be
+     * for example camel or panel)
+     * where the lines represent any letter and other letters must be in the given
+     * positions in the word.
+     */
     public WordList theWordsWithCharacters(String someString) {
         List<String> equalCharacterWords = new ArrayList<>();
         int stringLength = someString.length();
